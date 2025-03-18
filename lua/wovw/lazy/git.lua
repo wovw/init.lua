02010f22344ca115ee1b2b27f5e0cb4277fc7b4c
wovw <anthonypasala12@gmail.com>
2025-03-18 01:25:51 -0500

    i don't like blamer actually

--- lua/wovw/lazy/git.lua
+++ lua/wovw/lazy/git.lua
@@ -1,46 +1,38 @@
--- blamer config
-vim.g.blamer_enabled = true
-vim.g.blamer_delay = 500 -- ms
-vim.g.blamer_date_format = '%m/%d/%y %H:%M'
-
 return {
-    "APZelos/blamer.nvim",
-    {
-        "tpope/vim-fugitive",
-        config = function()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-            local wovw_Fugitive = vim.api.nvim_create_augroup("wovw_Fugitive", {})
+        local wovw_Fugitive = vim.api.nvim_create_augroup("wovw_Fugitive", {})
 
-            local autocmd = vim.api.nvim_create_autocmd
-            autocmd("BufWinEnter", {
-                group = wovw_Fugitive,
-                pattern = "*",
-                callback = function()
-                    if vim.bo.ft ~= "fugitive" then
-                        return
-                    end
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = wovw_Fugitive,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
 
-                    local bufnr = vim.api.nvim_get_current_buf()
-                    local opts = { buffer = bufnr, remap = false }
-                    vim.keymap.set("n", "<leader>pp", function()
-                        vim.cmd.Git('push')
-                    end, opts)
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = { buffer = bufnr, remap = false }
+                vim.keymap.set("n", "<leader>pp", function()
+                    vim.cmd.Git('push')
+                end, opts)
 
-                    -- rebase always
-                    vim.keymap.set("n", "<leader>P", function()
-                        vim.cmd.Git("pull --rebase")
-                    end, opts)
+                -- rebase always
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git("pull --rebase")
+                end, opts)
 
-                    -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                    -- needed if i did not set the branch up correctly
-                    vim.keymap.set("n", "<leader>pt", ":Git push -u origin ", opts);
-                end,
-            })
+                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+                -- needed if i did not set the branch up correctly
+                vim.keymap.set("n", "<leader>pt", ":Git push -u origin ", opts);
+            end,
+        })
 
-            vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit!<CR>")
-            vim.keymap.set("n", "gl", "<cmd>diffget //2<CR>")
-            vim.keymap.set("n", "gr", "<cmd>diffget //3<CR>")
-        end
-    }
+        vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit!<CR>")
+        vim.keymap.set("n", "gl", "<cmd>diffget //2<CR>")
+        vim.keymap.set("n", "gr", "<cmd>diffget //3<CR>")
+    end
 }
