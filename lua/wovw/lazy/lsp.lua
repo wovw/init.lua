c824572a5c5e49ffd72710673fc1542b26876afd
wovw <anthonypasala12@gmail.com>
2025-04-13 22:54:15 -0500

    refactor(lsp): ts_ls -> vtsls from lazyvim

--- lua/wovw/lazy/lsp.lua
+++ lua/wovw/lazy/lsp.lua
@@ -130,6 +130,11 @@ return {
 						{ server = opts }))
 					return false
 				end,
+				vtsls = function(_, opts)
+					-- copy typescript settings to javascript
+					opts.settings.javascript =
+						vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+				end,
 			},
 		},
 
@@ -228,7 +233,7 @@ return {
 					"pyright",
 					"basedpyright",
 
-					"ts_ls",
+					"vtsls",
 					"eslint",
 					"tailwindcss",
 					"prismals",
@@ -261,9 +266,98 @@ return {
 						vim.g.zig_fmt_parse_errors = 0
 						vim.g.zig_fmt_autosave = 0
 					end,
-					ts_ls = function()
-						nvim_lsp.ts_ls.setup({
+					vtsls = function(_, opts)
+						-- https://www.lazyvim.org/extras/lang/typescript
+						nvim_lsp.vtsls.setup({
 							capabilities = capabilities,
+							root_dir = require("lspconfig.util").root_pattern('.git', 'tsconfig.json', 'package.json',
+								'jsconfig.json'), -- find root from .git first for monorepo support
+							-- explicitly add default filetypes, so that we can extend them in related extras
+							filetypes = {
+								"javascript",
+								"javascriptreact",
+								"javascript.jsx",
+								"typescript",
+								"typescriptreact",
+								"typescript.tsx",
+							},
+							settings = {
+								complete_function_calls = true,
+								vtsls = {
+									enableMoveToFileCodeAction = true,
+									autoUseWorkspaceTsdk = true,
+									experimental = {
+										maxInlayHintLength = 30,
+										completion = {
+											enableServerSideFuzzyMatch = true,
+										},
+									},
+								},
+								typescript = {
+									updateImportsOnFileMove = { enabled = "always" },
+									suggest = {
+										completeFunctionCalls = true,
+									},
+									inlayHints = {
+										enumMemberValues = { enabled = true },
+										functionLikeReturnTypes = { enabled = true },
+										parameterNames = { enabled = "literals" },
+										parameterTypes = { enabled = true },
+										propertyDeclarationTypes = { enabled = true },
+										variableTypes = { enabled = false },
+									},
+								},
+							},
+							on_attach = function(client, _)
+								client.commands["_typescript.moveToFileRefactoring"] = function(command, _)
+									---@type string, string, lsp.Range
+									local action, uri, range = unpack(command.arguments)
+
+									local function move(newf)
+										client.request("workspace/executeCommand", {
+											command = command.command,
+											arguments = { action, uri, range, newf },
+										})
+									end
+
+									local fname = vim.uri_to_fname(uri)
+									client.request("workspace/executeCommand", {
+										command = "typescript.tsserverRequest",
+										arguments = {
+											"getMoveToRefactoringFileSuggestions",
+											{
+												file = fname,
+												startLine = range.start.line + 1,
+												startOffset = range.start.character + 1,
+												endLine = range["end"].line + 1,
+												endOffset = range["end"].character + 1,
+											},
+										},
+									}, function(_, result)
+										---@type string[]
+										local files = result.body.files
+										table.insert(files, 1, "Enter new path...")
+										vim.ui.select(files, {
+											prompt = "Select move destination:",
+											format_item = function(f)
+												return vim.fn.fnamemodify(f, ":~:.")
+											end,
+										}, function(f)
+											if f and f:find("^Enter new path") then
+												vim.ui.input({
+													prompt = "Enter move destination:",
+													default = vim.fn.fnamemodify(fname, ":h") .. "/",
+													completion = "file",
+												}, function(newf)
+													return newf and move(newf)
+												end)
+											elseif f then
+												move(f)
+											end
+										end)
+									end)
+								end
+							end,
 							handlers = {
 								["textDocument/publishDiagnostics"] = function(
 									_,
