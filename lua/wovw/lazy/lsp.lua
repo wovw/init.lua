f4894b4b8a4968ee785ba9b27b4512a00de803d1
wovw <anthonypasala12@gmail.com>
2025-05-23 11:06:49 -0500

    fix: better-ts-errors.nvim

--- lua/wovw/lazy/lsp.lua
+++ lua/wovw/lazy/lsp.lua
@@ -11,13 +11,14 @@ return {
 		},
 	},
 	{
-		"davidosomething/format-ts-errors.nvim",
-		config = function()
-			require("format-ts-errors").setup({
-				add_markdown = true, -- wrap output with markdown ```ts ``` markers
-				start_indent_level = 0, -- initial indent
-			})
-		end,
+		"OlegGulevskyy/better-ts-errors.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		config = {
+			keymaps = {
+				toggle = '<leader>td', -- default '<leader>dd'
+				go_to_definition = nil -- default '<leader>dx'
+			}
+		}
 	},
 	{
 		-- https://github.com/LazyVim/LazyVim/blob/ec5981dfb1222c3bf246d9bcaa713d5cfa486fbd/lua/lazyvim/plugins/extras/lang/rust.lua#L58
@@ -364,44 +365,6 @@ return {
 									end)
 								end
 							end,
-							handlers = {
-								["textDocument/publishDiagnostics"] = function(
-									_,
-									result,
-									ctx
-								)
-									if result.diagnostics == nil then
-										return
-									end
-
-									-- ignore some tsserver diagnostics
-									local idx = 1
-									while idx <= #result.diagnostics do
-										local entry = result.diagnostics[idx]
-
-										local formatter = require(
-											'format-ts-errors')[entry.code]
-										entry.message = formatter and
-											formatter(entry.message) or entry
-											.message
-
-										-- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-										if entry.code == 80001 then
-											-- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-											table.remove(result.diagnostics,
-												idx)
-										else
-											idx = idx + 1
-										end
-									end
-
-									vim.lsp.diagnostic.on_publish_diagnostics(
-										_,
-										result,
-										ctx
-									)
-								end,
-							},
 						})
 					end,
 					eslint = function()
