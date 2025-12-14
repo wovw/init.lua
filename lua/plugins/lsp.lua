ddc3f6b294d6a197230bc78f706ec073b13ea513
wovw <anthonypasala12@gmail.com>
2025-12-14 16:35:33 -0500

    ignore ts unused variable HINT

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,6 +12,26 @@ return {
           },
         },
       },
+      vtsls = {
+        handlers = {
+          -- Custom handler to filter out specific diagnostic codes
+          ["textDocument/publishDiagnostics"] = function(err, result, ctx)
+            if result and result.diagnostics then
+              local ignored_codes = { [6196] = true } -- Unused variables
+              local filtered = {}
+              for _, diagnostic in ipairs(result.diagnostics) do
+                if not (ignored_codes[diagnostic.code] and diagnostic.severity == 4) then
+                  table.insert(filtered, diagnostic)
+                  -- else
+                  --   print("Ignoring diagnostic: " .. vim.inspect(diagnostic))
+                end
+              end
+              result.diagnostics = filtered
+            end
+            vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
+          end,
+        },
+      },
     },
   },
 }
