9655b69d5910f696192d7a02bce0afba4bbc76f2
wovw <anthonypasala12@gmail.com>
2026-01-08 10:24:03 -0500

    no flash on `/` actually

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -55,15 +55,4 @@ return {
       })
     end,
   },
-  {
-    "folke/flash.nvim",
-    ---@type Flash.Config
-    opts = {
-      modes = {
-        search = {
-          enabled = true,
-        },
-      },
-    },
-  },
 }
