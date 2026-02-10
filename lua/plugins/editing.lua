734c1660425ea1a636e4f4f3b5136869552ac08a
wovw <anthonypasala12@gmail.com>
2026-02-09 20:43:08 -0500

    css color previews

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -55,4 +55,21 @@ return {
       })
     end,
   },
+  {
+    "eero-lehtinen/oklch-color-picker.nvim",
+    event = "VeryLazy",
+    version = "*",
+    keys = {
+      -- One handed keymap recommended, you will be using the mouse
+      {
+        "<leader>vv",
+        function()
+          require("oklch-color-picker").pick_under_cursor()
+        end,
+        desc = "Color pick under cursor",
+      },
+    },
+    ---@type oklch.Opts
+    opts = {},
+  },
 }
