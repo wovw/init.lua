977c4fd4b0c565179eda391bfa2a62cbc4b9e87c
wovw <anthonypasala12@gmail.com>
2026-04-08 02:43:33 -0400

    no supermaven on large files

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,6 +7,20 @@ return {
         clear_suggestion = "<C-]>",
         accept_word = "<C-h>",
       },
+      condition = function()
+        local filename = vim.api.nvim_buf_get_name(0)
+
+        -- Check if the file exists on disk (prevents errors on new, unsaved buffers)
+        local stats = vim.uv.fs_stat(filename)
+        if stats then
+          local size_limit = 1024 * 1024 -- 1 MB
+          if stats.size > size_limit then
+            return true -- Stop Supermaven
+          end
+        end
+
+        return false -- Continue Supermaven
+      end,
     },
   },
   {
